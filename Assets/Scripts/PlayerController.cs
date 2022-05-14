87b0d733ee3a8dbff6f95d346eecdc6604f682b3
Fumitaka Tokumitsu <toku345@gmail.com>
2022-05-14 17:09:31 +0900

    バンをまっすぐ走らせる

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // move the vehicle forward
+        transform.Translate(0, 0, 1);
     }
 }
