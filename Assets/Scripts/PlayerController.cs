f493053b14d7da1cb916c3c6d486d97e4c6bf029
Fumitaka Tokumitsu <toku345@gmail.com>
2022-05-25 07:02:55 +0900

    <-(a) / ->(d) キーで左右に移動できるようにした

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float turnSpeed;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
