ca351fa3d0ab29cbc3753dd7eed0b1a15fd82f8c
Fumitaka Tokumitsu <toku345@gmail.com>
2022-05-27 07:01:29 +0900

    バンをスライドさせずに回転させる

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,6 +22,6 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
